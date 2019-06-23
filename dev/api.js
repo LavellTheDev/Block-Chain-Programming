90f4d994a8247da86305b0485491362d98582fa7
Lavell Wesley <lavellthedev+github@gmail.com>
2019-06-23 05:10:54 -0400

    Building the blockchain endpoint

--- dev/api.js
+++ dev/api.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('blockchain', function (req, res){
+app.get('/blockchain', function (req, res){
+  res.send(bitcoin);
 
 });
+
 
 app.post('/transaction', function(req, res){
   console.log(req.body);
